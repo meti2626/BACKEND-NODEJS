165879427ce19cca67db14752a9a30972a08cb03
meti2626 <metseboka@gmail.com>
2025-11-19 23:21:38 +0300

    Nov-19-2

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -5,9 +5,12 @@ const app= express()
 const PORT = 8383
 
 
-let data ={
-    name: 'james'
- }
+let data =['james']
+ 
+//Middleware
+app.use(express.json())
+
+
 
 
 //HTTP VERBS and Routes
@@ -15,19 +18,52 @@ let data ={
 
 //Website endpoints -[for sending back  html and they typically come when a user enters  a url in a browser ]
 app.get('/', (req,res)=>{
-     res.send('<h1>HomePage</h1>')
+    console.log('This is home page')
+     res.send(`
+        <body style="background:pink ; color:blue;">
+            <h1>DATA:</h1>
+             <p>${JSON.stringify(data)}</p>
+            <a href="/dashboard"> Dashboard</a>  
+        </body>
+    <script>console.log('This is home page')</script>
+        `)
 })
 
 app.get('/dashboard' , (req,res)=>{
     
-    res.send('<h1>DashBoard</h1>')
+    res.send(`
+         <body>
+         <h1>dashboard</h1>
+         <a href="/">home</a>
+         </body>
+        `)
 })
 
 //Type 2- API endpoints -[ when u enter username and password]
+// they typically send back data in json format
 
-app.get('/api/users' , (req,res) =>{
+//CRUD - CREATE-post , READ-get , UPDATE-put , DELETE-delete
+
+
+app.get('/api/data' , (req,res) =>{
     console.log('This one is for data')
     res.send(data)
+})
+
+app.post('/api/data' ,(req , res) =>{
+    // someone wants to create a user (when they click a sign up )
+// the user cicks the sign up button after entering credentials , and thie browser is wires up tosend out a network request to the server to handle that action 
+
+     const newEntry = req.body
+     console.log(newEntry)
+     data.push(newEntry.name)
+     res.sendStatus(201)
+       })
+
+app.delete('/api/data' , (req,res)=>{
+    data.pop()
+    console.log('deleted ')
+    res.sendStatus(203)
 })
 
 
