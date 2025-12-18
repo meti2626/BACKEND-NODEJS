9dfdd05b6327b99e8595329413afa215f83356a1
meti2626 <metseboka@gmail.com>
2025-12-18 23:32:15 +0300

    dec-18-2

---
+++ 2-express-tutorial/routes/auth.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/' , (req,res)=>
+{
+   const {name}  = req.body;
+   if(name){
+      return res.status(200).send(`welcome ${name}`)
+   }
+    res.status(401).send('please Provide Credential')
+})
+
+
+module.exports = router;
